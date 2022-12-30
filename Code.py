9268ada4f026257455687ad35474ff56e892997f
Whitedevil <105511358+whitedevil222@users.noreply.github.com>
2022-12-30 21:37:53 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,11 @@
+import turtle as t
+t.bgcolor("black")
+t.pencolor("yellow")
+t.pensize("8")
+t.up()
+t.goto(-200,0)
+t.down()
+for i in range(5):
+    t.forward(400)
+    t.right(144)
+t.hideturtle
