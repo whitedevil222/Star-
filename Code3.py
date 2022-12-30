60f32fe78e8e3058a76ee81a4ca74eaf5f30e5cb
Whitedevil <105511358+whitedevil222@users.noreply.github.com>
2022-12-30 21:46:34 +0530

    Add files via upload

---
+++ Code3.py
@@ -0,0 +1,14 @@
+import turtle
+
+colors = ['yellow', 'green', 'red', 'white', 'cyan', 'blue']
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.bgcolor('black')
+t.speed(30)
+
+for i in range(100):
+    t.color(colors[i%6])
+    t.fd(i*5)
+    t.left(200)
+    t.width(2)
